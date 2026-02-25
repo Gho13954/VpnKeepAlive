03153353073b4686ab958891be7b085809cc1495
WJH <wujiahao@hichain.com>
2026-02-25 15:19:24 +0800

    fix: ping多次bug修复

--- PingSettings.cs
+++ PingSettings.cs
@@ -4,7 +4,7 @@ public class PingSettings
 {
     public const string SectionName = "PingSettings";
 
-    public List<string> TargetHosts { get; set; } = new() { "192.168.1.102" };
+    public List<string> TargetHosts { get; set; } = new();
     public int MinIntervalSeconds { get; set; } = 30;
     public int MaxIntervalSeconds { get; set; } = 60;
     public int TimeoutMilliseconds { get; set; } = 3000;
